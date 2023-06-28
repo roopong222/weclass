1e06c176b9b085303bcd018f3096459083e721a9
roopong222 <roopong28@naver.com>
2023-06-28 18:12:53 +0900

    0628

--- 41.svg/js/study3.js
+++ 41.svg/js/study3.js
@@ -1,24 +1,53 @@
 $(document).ready(function(){
-    $(`.btn`).click(function(){
-        if($(`.skill`).hasClass(`on`)==false){
-            //스킬클래스에 on클래스가 없다면 실행
-            $(`.skill`).addClass(`on`)
-            $(`.skill>li`).each(function(){
-                let list = $(this)
-                let percent = $(this).find(`.per`).text()
-                let count = 0
-                let circle = $(this).find(`circle`)
+    // $(`.btn`).click(function(){
+    //     if($(`.skill`).hasClass(`on`)==false){
+    //         //스킬클래스에 on클래스가 없다면 실행
+    //         $(`.skill`).addClass(`on`)
+    //         $(`.skill>li`).each(function(){
+    //             let list = $(this)
+    //             let percent = $(this).find(`.per`).text()
+    //             let count = 0
+    //             let circle = $(this).find(`circle`)
 
-                let timer = setInterval(function(){
-                    count++
-                    list.find(`.per`).text(count+`%`)
-                    circle.css(`stroke-dashoffset`,314-(314*(count/100)))
-                    if(count>=percent){
-                        clearInterval(timer)
-                    }
-                },1);
-            })
-        }
+    //             let timer = setInterval(function(){
+    //                 count++
+    //                 list.find(`.per`).text(count+`%`)
+    //                 circle.css(`stroke-dashoffset`,314-(314*(count/100)))
+    //                 if(count>=percent){
+    //                     clearInterval(timer)
+    //                 }
+    //             },10);
+    //         })
+    //     }
         
+    // })
+
+
+    let skilltop = $(`.skill`).offset().top
+    $(window).scroll(function(){
+        let wtop = $(window).scrollTop()
+        let wheight = $(window).height()/2
+        if(wtop+wheight >= skilltop){
+            if($(`.skill`).hasClass(`on`)==false){
+                //스킬클래스에 on클래스가 없다면 실행
+                $(`.skill`).addClass(`on`)
+                $(`.skill>li`).each(function(){
+                    let list = $(this)
+                    let percent = $(this).find(`.per`).text()
+                    let count = 0
+                    let circle = $(this).find(`circle`)
+    
+                    let timer = setInterval(function(){
+                        count++
+                        list.find(`.per`).text(count+`%`)
+                        circle.css(`stroke-dashoffset`,314-(314*(count/100)))
+                        if(count>=percent){
+                            clearInterval(timer)
+                        }
+                    },10);
+                })
+            }
+        }
     })
+    
 })
