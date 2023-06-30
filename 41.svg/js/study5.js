1384f23da169da4574601f38fcdf5df47aafed94
roopong222 <roopong28@naver.com>
2023-06-30 18:11:38 +0900

    0630

--- 41.svg/js/study5.js
+++ 41.svg/js/study5.js
@@ -1,34 +1,40 @@
 $(document).ready(function(){
 
+    let pathLength = $(`#progress path`).get(0).getTotalLength()
+
     let Frametop = $(`.fixedFrame`).offset().top
     let contop = $(`.contents`).offset().top
     let moAreah = $(`.movingArea`).height()
     let introh = $(`.intro`).height()
     let inconh = contop-introh-moAreah
 
-    let pathLength = $(`#progress path`).get(0).getTotalLength()
-
     $(`#progress path`).css(`stroke-dasharray`,pathLength)
     $(`#progress path`).css(`stroke-dashoffset`,pathLength)
 
     $(window).scroll(function(){
         let wintop = $(window).scrollTop()
+        let dstens = wintop-Frametop
 
-
-        if(wintop-Frametop<0){
+        if(dstens<0){
             $(`.movingArea`).css(`position`,`absolute`)
             $(`.movingArea`).css(`left`,`0`)
             $(`.movingArea`).css(`top`,`0`)
         }
-        if(wintop-Frametop>0 && wintop-Frametop<inconh){
+        if(dstens>0 && dstens<inconh){
             $(`.movingArea`).css(`position`,`fixed`)
             $(`.movingArea`).css(`top`,`0`)
             $(`.movingArea`).css(`left`,`0`)
-           let per = ((wintop-Frametop)/inconh*100)
-           $(`#progress path`).css(`stroke-dashoffset`,pathLength-(pathLength*per/100))
-           $(`.per`).text(Math.round(per)+`%`)
+            
+            let per = (dstens/inconh*100) // 0~100까지의 수
+            $(`#progress path`).css(`stroke-dashoffset`,pathLength-(pathLength*per/100))
+            $(`.per`).text(Math.round(per)+`%`)
 
-        }else if(wintop-Frametop>inconh){
+            let per2 = (dstens/inconh*87.5) // 0~87.5 까지의 수
+            $(`.train`).css(`transform`,`translateX(-${per2}%)`)
+
+
+
+        }else if(dstens>inconh){
             $(`.movingArea`).css(`position`,`absolute`)
             $(`.movingArea`).css(`left`,`0`)
             $(`.movingArea`).css(`top`,`${inconh}px`)
