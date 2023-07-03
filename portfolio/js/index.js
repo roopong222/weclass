788ab8dcaba53aaf77ed52ddded3c663d8c47a80
roopong222 <roopong28@naver.com>
2023-07-03 20:44:16 +0900

    0703

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    const sliderswiper = new Swiper('.popupstation', {
+    const popupstation = new Swiper('.popupstation', {
         slidesPerView: 3,
         spaceBetween: 55,
         pagination: {
@@ -11,9 +11,22 @@ $(document).ready(function(){
             prevEl: ".swiper-button-prev",
         },
     });
+    const bannerstation = new Swiper('.bannerstation', {
+        slidesPerView: 1,
+        // spaceBetween: 10,
+        loop: true,
+        pagination: {
+            el: ".bannerstation-pagination",
+            type: "progressbar",
+        },
+    });
+
     $(`.popuptrain>li`).click(function(e){
         e.preventDefault()
         $(`.popuplayout`).addClass(`on`)
+        let count = ($(this).index())
+        $(`.popuplayoutdl>dt`).removeClass(`on`)
+        $(`.popuplayoutdl>dt`).eq(count).addClass(`on`)
     })
 
     $(`.pclose`).click(function(e){
@@ -26,4 +39,19 @@ $(document).ready(function(){
         $(`.popuplayoutdl>dt`).removeClass(`on`)
         $(this).addClass(`on`)
     })
+
+    $(`.bannerlayoutul>li`).click(function(e){
+        e.preventDefault()
+        $(`.bannerstation`).addClass(`on`)
+        let count2 = $(this).index()
+        bannerstation.slideToLoop(count2, 500)
+    })
+
+    $(`.bclose`).click(function(e){
+        e.preventDefault()
+        $(`.bannerstation`).removeClass(`on`)
+    })
+
+    
+
 })
